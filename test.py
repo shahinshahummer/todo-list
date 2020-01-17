2bb4499e17e5b3f13fada71b320d39e765b6a140
Shahinshah <shahinshaummer83@gmail.com>
2020-01-17 14:31:33 +0530

    list and scheduled list added

--- test.py
+++ test.py
@@ -1,23 +1,58 @@
 
-from todolist import addline
+from todolist import addtask,scheduled_task,get_list
 
-def test_add_line():
+def test_add_task():
     dict = {}
     todo = {'2020-01-15': ["pay rent"]}
-    assert addline(dict,'2020-01-15', "pay rent") == todo
+    assert addtask(dict,'2020-01-15', "pay rent") == todo
 
-def test_add_2nd_line():
-    dict = {}
-    todo = {'2020-01-15': ["pay rent"]}
-    addline(dict,'2020-01-15', "pay rent") == todo
-    
+def test_add_task_2nd_day():
+    dict = {'2020-01-15': ["pay rent"]}
     todo = {'2020-01-15': ["pay rent"], '2020-01-16': ["pay tax"]}
-    assert addline(dict,'2020-01-16', "pay tax") == todo
+    assert addtask(dict,'2020-01-16', "pay tax") == todo
  
-def test_add_2nd_value():
-    dict = {}
-    todo = {'2020-01-15': ["pay rent"]}
-    addline(dict,'2020-01-15', "pay rent") == todo
-    
+def test_add_2nd_task_same_date():
+    dict = {'2020-01-15': ["pay rent"]}    
     todo = {'2020-01-15': ["pay rent", "pay tax"]}
-    assert addline(dict,'2020-01-15', "pay tax") == todo
+    assert addtask(dict,'2020-01-15', "pay tax") == todo
+
+
+def test_get_list():
+    dict = {'2020-01-15': ["pay rent"], '2020-01-16': ["pay tax"], '2020-01-17': ["household work"],
+            '2020-01-18': ["long drive"], '2020-01-19': ["presentation"],
+            '2020-01-20': ["book a movie"],'2020-01-21': ["date lilly"],
+            '2020-01-22': ["checkup"], '2020-01-23': ["festival"]}
+    assert get_list(dict) == """2020-01-15
+pay rent
+lol
+2020-01-16
+pay tax
+2020-01-17
+household work
+2020-01-18
+long drive
+2020-01-19
+presentation
+2020-01-20
+book a movie
+2020-01-21
+date lilly
+2020-01-22
+checkup
+2020-01-23
+festival """
+
+
+
+
+
+
+    
+def test_get_scheduled_list():
+    dict = {'2020-01-15': ["pay rent"], '2020-01-17': ["pay tax"]}
+    assert scheduled_task(dict) == dict['2020-01-17']
+
+
+def test_get_scheduled_list_blank():
+    dict = {}
+    assert scheduled_task(dict) == "no tasks today"
--- todolist.py
+++ todolist.py
@@ -4,10 +4,27 @@ from datetime import date
 
 dict = {}
 
-def addline(dict,date,value):
+def addtask(dict,date,value):
     if date in dict: 
         dict[date].append(value)
     else:
         dict[date] = [value]
     return (dict)
 
+def get_list(dict):
+    for x in dict:
+        print (str(x))
+        for y in dict[x]:
+            print (str(y))
+
+
+def scheduled_task(dict):
+    if len(dict) != 0:
+        currentdate = date.today()
+        return(dict[str(currentdate)])
+    else:
+        return("no tasks today")
+
+
+
+    
